22025f1a9af45ed752a870b0bef51352f5fe6201
Harman Singh <55681620+harmansingh25@users.noreply.github.com>
2022-01-10 22:45:19 +0530

    Add files via upload

---
+++ street-lights-app/src/Components/Input.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+function Input({setSrcDest, plot, setPlot}){
+    const sLatRef = React.useRef(null);
+    const sLongRef = React.useRef(null);
+    const dLatRef = React.useRef(null);
+    const dLongRef = React.useRef(null);
+
+    return (<div className = "Input">
+            <label>Enter Source</label>
+            <input ref={sLatRef} type = "text" className = "SrcLat" placeholder="Latitude"/>
+            <input ref = {sLongRef} type = "text" className = "SrcLong" placeholder="Longitude"/>
+            <br /> <br />
+            <label>Enter Destination</label>
+            <input ref = {dLatRef} type = "text" className = "DestLat" placeholder="Latitude"/>
+            <input ref = {dLongRef} type = "text" className = "DestLong" placeholder="Longitude"/>
+            <br /> <br />
+            <button onClick={()=>{
+      
+                setSrcDest({srcLat:sLatRef.current.value, srcLong:sLongRef.current.value, destLat:dLatRef.current.value, destLong:dLongRef.current.value });
+                setPlot(plot+1);
+            }}>Plot</button>
+            
+            
+        </div>
+    );
+
+}
+
+
+export default Input;
