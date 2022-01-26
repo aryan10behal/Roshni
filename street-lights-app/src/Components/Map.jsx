62af39ef2686974327561889ccf7babc0111c466
Harman Singh <55681620+harmansingh25@users.noreply.github.com>
2022-01-27 02:06:23 +0530

    Plotting dark routes with infoWindow

--- street-lights-app/src/Components/Map.jsx
+++ street-lights-app/src/Components/Map.jsx
@@ -1,14 +1,17 @@
+
 import React from 'react';
 import {KmeansAlgorithm, MarkerClusterer} from '@googlemaps/markerclusterer'
   
 
-function Map({children, style, className, center, zoom, markerPositions, route, heatmapData, bounds}) {
+function Map({children, style, className, center, zoom, markerPositions, route, heatmapData, bounds, darkroutes, darkbounds, darkDistances}) {
 
     const ref = React.useRef(null);
     const [map, setMap] = React.useState();
     const clusterer = React.useRef();
     const heatmap = React.useRef();
     const routePlot = React.useRef();
+    
+    // const darkRoutePlot = React.useRef([]);
 
     React.useEffect(() => {
         if (ref.current && !map) {
@@ -23,9 +26,10 @@ function Map({children, style, className, center, zoom, markerPositions, route, 
     }, [map, center, zoom])
 
     React.useEffect(() => {
-        var markers = markerPositions.map(position => new window.google.maps.Marker({position, icon: "http://localhost:8000/icon"}));
-        if(clusterer.current) clusterer.current.setMap(null);
-        clusterer.current = new MarkerClusterer({
+        if(!darkDistances){
+            var markers = markerPositions.map(position => new window.google.maps.Marker({position, icon: "http://localhost:8000/icon"}));
+            if(clusterer.current) clusterer.current.setMap(null);
+            clusterer.current = new MarkerClusterer({
             map, 
             markers, 
             algorithm: new KmeansAlgorithm({
@@ -33,6 +37,9 @@ function Map({children, style, className, center, zoom, markerPositions, route, 
                 numberOfClusters: (count, zoom) => count < 200 ? count : Math.max(1, zoom - 8)
             })
         });
+
+        }
+        
     }, [map, markerPositions])
 
     React.useEffect(() => {
@@ -40,6 +47,68 @@ function Map({children, style, className, center, zoom, markerPositions, route, 
         if(heatmap.current) heatmap.current.setMap(null);
         heatmap.current = new window.google.maps.visualization.HeatmapLayer({map, data: heatmapData});
     }, [map, heatmapData])
+
+    
+    React.useEffect(() => {
+        if(!darkroutes || !darkroutes.length || !darkbounds) return;
+      
+       
+        var darkRoutesFinal = []
+        for(let i=0;i<darkroutes.length;i++){
+            var darkRoutesTemp = []
+            for(let j=0; j<darkroutes[i].length; j++){
+                darkRoutesTemp[j] = new window.google.maps.LatLng(darkroutes[i][j])
+            }
+            darkRoutesFinal[i]=darkRoutesTemp;
+        }
+        
+        for(let i = 0; i<darkRoutesFinal.length;i++){
+
+           
+            var darkPath = new window.google.maps.Polyline({
+                path: darkRoutesFinal[i],
+                geodesic: true,
+                strokeColor: "#FF0000",
+                strokeOpacity: 1.0,
+                strokeWeight: 4,
+                });
+        
+                darkPath.setMap(map);
+
+        }
+       
+       
+        for(let i=0; i<darkroutes.length; i++){
+            const marker1 = new window.google.maps.Marker({
+                position: darkroutes[i][0],
+                icon: "http://localhost:8000/icon",
+                map
+              });
+              const marker2 = new window.google.maps.Marker({
+                position: darkroutes[i][darkroutes[i].length-1],
+                icon: "http://localhost:8000/icon",
+                map
+              });
+
+             
+                const contentString = `<div id="content"><p>Distance to the next pole <br><h1>${darkDistances[i]} </h1></div>`;;
+                const infowindow = new window.google.maps.InfoWindow({
+                    content: contentString
+    
+                  });
+                  
+                  new window.google.maps.event.addListener(marker1, 'click', function () {
+                    infowindow.open(map, marker1);
+                });
+
+              
+            
+
+        }
+
+  
+    }, [map, darkroutes, darkbounds, markerPositions])
+
 
     React.useEffect(() => {
         if(!route || !route.length || !bounds) return;
@@ -80,6 +149,3 @@ export default Map;
 // 28.595233929736626 77.08882146289189
 // 28.59586331072582 77.08783862983469
 // 28.59610935666011 77.08881740159825
-
-
-
