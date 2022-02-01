b9fe83668b58f82100f5eb2082e037041bcf0ef4
Harman Singh <55681620+harmansingh25@users.noreply.github.com>
2022-02-01 22:15:07 +0530

    Added sliders and dark routes input

--- street-lights-app/src/Components/Input.jsx
+++ street-lights-app/src/Components/Input.jsx
@@ -2,10 +2,16 @@ import React from "react";
 import Button from '@mui/material/Button';
 import { Checkbox, CircularProgress, FormControlLabel, Slider, TextField, Typography } from "@mui/material";
 
-function Input({setShowAllStreetlights, setShowHeatmap, showRoute, setShowRoute, setSrc, setDest, fetchRouteData, loading}){
-    const MIN_THRESH = 50;
-    const MAX_THRESH = 1000;
-    const [darkStretchThreshold, setDarkStretchThreshold] = React.useState(MIN_THRESH)
+function Input({setShowAllStreetlights, setShowHeatmap, showRoute, setShowRoute, setSrc, setDest, setSrcDark, setDestDark, fetchRouteData, fetchDarkRouteData, loading, showDarkRoute, setDarkRoute,
+    distanceFromStreet,
+    darkStretchThreshold,
+    setDistanceFromStreet,
+    setDarkStretchThreshold,
+    MIN_THRESH_DARK,
+    MAX_THRESH_DARK,
+    MIN_THRESH_ROUTE,
+    MAX_THRESH_ROUTE}){
+    
 
     return (
         <div className="Input">
@@ -14,20 +20,37 @@ function Input({setShowAllStreetlights, setShowHeatmap, showRoute, setShowRoute,
                 <FormControlLabel control={<Checkbox onChange={(e) => setShowHeatmap(e.target.checked)} />} label="Heatmap" />
                 <FormControlLabel control={<Checkbox onChange={(e) => setShowAllStreetlights(e.target.checked)} />} label="All Street Lights" />
                 <FormControlLabel control={<Checkbox onChange={(e) => setShowRoute(e.target.checked)} />} label="Street Lights on Route" />
-            </div>
+                
+            
             {showRoute? (
                 <div className="Layers">
                     <TextField className="Input-Field" variant="standard" label="Source" onChange={(e) => setSrc(e.target.value)} type = "text"/>
                     <TextField className="Input-Field" variant="standard" label="Destination" onChange={(e) => setDest(e.target.value)} type = "text"/>
-                    {/* <Typography className="Input-Field" component="div">
-                        Dark Stretch Threshold: {darkStretchThreshold}m
+                    <Typography className="Input-Field" component="div">
+                        Distance from the street: {distanceFromStreet}m
                     </Typography>
-                    <Slider valueLabelDisplay="auto" min={MIN_THRESH} max={MAX_THRESH} onChange={(event) => setDarkStretchThreshold(event.target.value)}/> */}
+                    <Slider valueLabelDisplay="auto" min={MIN_THRESH_ROUTE} max={MAX_THRESH_ROUTE} onChange={(event) => setDistanceFromStreet(event.target.value)}/>
                     <Button className="Input-Field" onClick={()=>{
                         fetchRouteData()
                     }}>Load</Button>
                 </div>
-            ) : ""}     
+            ) : ""}
+            <FormControlLabel control={<Checkbox onChange={(e) => setDarkRoute(e.target.checked)} />} label="Dark Stretches on Route" />
+            {showDarkRoute? (
+                <div className="Layers">
+                    <TextField className="Input-Field" variant="standard" label="Source_Dark" onChange={(e) => setSrcDark(e.target.value)} type = "text"/>
+                    <TextField className="Input-Field" variant="standard" label="Destination_Dark" onChange={(e) => setDestDark(e.target.value)} type = "text"/>
+                    <Typography className="Input-Field" component="div">
+                        Dark Stretch Threshold: {darkStretchThreshold}m
+                    </Typography>
+                    <Slider valueLabelDisplay="auto" min={MIN_THRESH_DARK} max={MAX_THRESH_DARK} onChange={(event) => setDarkStretchThreshold(event.target.value)}/>
+                    <Button className="Input-Field" onClick={()=>{
+                        fetchDarkRouteData()
+                    }}>Load</Button>
+                </div>
+            ) : ""}
+            
+            </div>     
             <div style={{display: loading ? "flex" :  "none"}} className="Loading">
                 <CircularProgress />
             </div>      
