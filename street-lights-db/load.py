f19b41461cae20a3f85c45877796756f3c10c1c3
Harman Singh <55681620+harmansingh25@users.noreply.github.com>
2022-02-14 04:46:28 +0530

    Handling newly added/deleted data

--- street-lights-db/load.py
+++ street-lights-db/load.py
@@ -23,10 +23,14 @@ files = ['./data/Najafgarh-1.csv', './data/Najafgarh-2.csv',
 	'./data/Central-1.csv', './data/Central-2.csv', './data_new/Najafgarh-1.csv', './data_new/Najafgarh-2.csv', 
 	'./data_new/South-1.csv', './data_new/South-2.csv', 
 	'./data_new/West-1.csv', './data_new/West-2.csv', './data_new/West-3.csv', './data_new/West-4.csv',
-	'./data_new/Central-1.csv', './data_new/Central-2.csv', './data_new/West-12.csv','./data_new/West-22.csv']
+	'./data_new/Central-1.csv', './data_new/Central-2.csv', './data_new/West-12.csv','./data_new/West-22.csv', './data/Added Lights.csv']
+
 
 lampposts = []
 streetlights_dfs = []
+
+deletedLights = pandas.read_csv('./data/Deleted Lights.csv')
+
 
 for file in files:
     streetlights_dfs.append(pandas.read_csv(file))
@@ -35,6 +39,10 @@ df_final = pandas.concat(streetlights_dfs)
 df_final_latlng = df_final[['Longitude', 'Latitude']]
 df_final_latlng = df_final_latlng.drop_duplicates(keep= 'last')
 df_final_latlng = df_final_latlng.dropna()
+
+df_final_latlng = pandas.concat([df_final_latlng, deletedLights, deletedLights]).drop_duplicates(keep=False)
+df_final_latlng = df_final_latlng.dropna()
+
 temp = df_final_latlng.values.tolist()
 temp = map(lambda x : {'lng': x[0], 'lat': x[1]}, temp)
 lampposts += temp
@@ -46,6 +54,7 @@ lampposts += temp
 #     temp = df.values.tolist()
 #     temp = map(lambda x : {'lng': x[0], 'lat': x[1]}, temp)
 #     lampposts += temp
+
 
 
 streetlights.insert_many(lampposts)
