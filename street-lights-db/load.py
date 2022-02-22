f527f52504ebebbecc15c438257c9ecbd3d12e9b
Harman Singh <55681620+harmansingh25@users.noreply.github.com>
2022-02-23 03:56:43 +0530

    Loaded additional columns

--- street-lights-db/load.py
+++ street-lights-db/load.py
@@ -38,15 +38,17 @@ for file in files:
     streetlights_dfs.append(pandas.read_csv(file))
 
 df_final = pandas.concat(streetlights_dfs)
-df_final_latlng = df_final[['Longitude', 'Latitude']]
+df_final_latlng = df_final[['Longitude', 'Latitude', 'CCMS NO', 'Zone', 'Type of Light', 'No. Of Lights', 'Ward No.' ,'Connected Load']]
 df_final_latlng = df_final_latlng.drop_duplicates(keep= 'last')
-df_final_latlng = df_final_latlng.dropna()
+df_final_latlng = df_final_latlng.dropna(subset=['Longitude', 'Latitude'])
 
 df_final_latlng = pandas.concat([df_final_latlng, deletedLights, deletedLights]).drop_duplicates(keep=False)
-df_final_latlng = df_final_latlng.dropna()
+df_final_latlng = df_final_latlng.dropna(subset=['Longitude', 'Latitude'])
+
+df_final_latlng = df_final_latlng.fillna('')
 
 temp = df_final_latlng.values.tolist()
-temp = map(lambda x : {'lng': x[0], 'lat': x[1]}, temp)
+temp = map(lambda x : {'lng': x[0], 'lat': x[1],'CCMS NO':x[2], 'Zone':x[3], 'Type of Light':x[4], 'No. Of Lights':x[5], 'Ward No.':x[6] ,'Connected Load':x[7] }, temp)
 lampposts += temp
 
 # for file in files:
