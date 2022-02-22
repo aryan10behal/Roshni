01b7ac4dad4ca740ae0361c75943f38e393a7007
aryan10behal <55681065+aryan10behal@users.noreply.github.com>
2022-02-22 23:54:02 +0530

    UserProvider added

--- street-lights-app/src/index.js
+++ street-lights-app/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { UserProvider } from "./context/UserContext";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+      <UserProvider>
+      <App />
+      </UserProvider>
+     </React.StrictMode>,
   document.getElementById('root')
 );
 
